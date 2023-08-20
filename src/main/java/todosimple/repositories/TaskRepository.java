36bb388a5691d5f1032d9ed74cbe532b5b648656
euphorialol <nandomagalhaes074@gmail.com>
2023-08-19 21:23:40 -0300

    Create User and Task Repositories

---
+++ src/main/java/todosimple/repositories/TaskRepository.java
@@ -0,0 +1,12 @@
+package todosimple.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import todosimple.models.Task;
+
+import java.util.List;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long> {
+    List<Task> findByUser_Id(Long id);
+}
